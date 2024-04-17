dc03b85823a09601eb8dca9058f2bc1d6d811a8c
malik <malikimenov@gmail.com>
2024-04-18 02:28:27 +0600

    added the configuration

--- backend/app.js
+++ backend/app.js
@@ -1,24 +1,25 @@
-require('./src/db/connect');
-const express = require('express');
-const fileUpload = require('express-fileupload');
-const config = require('../msdata/config.json');
-const router = require('./src/routes/index');
-const path = require('path');
+require("./src/db/connect");
+const express = require("express");
+const fileUpload = require("express-fileupload");
+const config = require("./msdata/config.json");
+const router = require("./src/routes/index");
+const path = require("path");
+const cors = require("cors");
 const PORT = config.port;
 
-
 const app = express();
+app.use(cors());
 app.use(express.json());
-app.use(express.static(path.resolve(__dirname, 'msdata/static')));
-app.use(fileUpload({}))
-app.use('/route', router);
+app.use(express.static(path.resolve(__dirname, "msdata/static")));
+app.use(fileUpload({}));
+app.use("/route", router);
 
 const start = async () => {
-    try {
-        app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
-    } catch (error) {
-        throw Error(error);
-    }
+  try {
+    app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+  } catch (error) {
+    throw Error(error);
+  }
 };
 
 start();
