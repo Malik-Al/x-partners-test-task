2816ee49e269e75995054b03f4da570d1d39e550
malik <malikimenov@gmail.com>
2024-04-18 15:52:52 +0600

    added error name

--- backend/src/middleware/error.handling.js
+++ backend/src/middleware/error.handling.js
@@ -4,5 +4,5 @@ module.exports = function (error, req, res, next){
     if(error instanceof ApiError){
         return res.status(error.status).json({message: error.message, errors: error.errors})
     }
-    return res.status(500).json({message: 'Непредвиденнвя ошибка!'})
+    return res.status(500).json({message: 'Unexpected error!'})
 }
