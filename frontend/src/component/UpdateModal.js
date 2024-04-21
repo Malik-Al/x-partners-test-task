1eab7c34e925819b07907b2b6606559a4deeec8c
malik <malikimenov@gmail.com>
2024-04-21 20:50:26 +0600

    added img profile

--- frontend/src/component/UpdateModal.js
+++ frontend/src/component/UpdateModal.js
@@ -1,8 +1,10 @@
-import {useState} from "react";
+import { useState } from "react";
 import { Box, Button } from "@mui/material";
-import {Modal, Typography} from "@mui/material";
+import { Modal, Typography } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
+import TextField from "@mui/material/TextField";
+import { Grid } from "@mui/material";
 // import axios from "axios";
 
 const style = {
@@ -21,8 +23,8 @@ const style = {
 
 export default function UpdateModal() {
   const users = useSelector((state) => state.users.users);
-  const user = users.filter(el => el._id === localStorage.getItem('id'))[0];
-  console.log('user', user);
+  const user = users.filter((el) => el._id === localStorage.getItem("id"))[0];
+  console.log("user", user);
 
   let navigate = useNavigate();
   const [open, setOpen] = useState(true);
@@ -30,7 +32,6 @@ export default function UpdateModal() {
   //   const handleOpen = () => {
   //     setOpen(true);
   //   };
-
 
   const handleClose = () => {
     setOpen(false);
@@ -44,10 +45,75 @@ export default function UpdateModal() {
         aria-labelledby="parent-modal-title"
         aria-describedby="parent-modal-description"
       >
-        <Box sx={{ ...style, width: 400 }}>
+        {/* <Box sx={{ ...style, width: 400 }}>
           <h2 id="parent-modal-title">Имя: {user.name}</h2>
           <Typography id="parent-modal-title">Пароль: {user.password}</Typography>
           <Button onClick={handleClose}>Close</Button>
+        </Box> */}
+
+        <Box sx={style}>
+          <form onSubmit>
+            <Grid container spacing={2} justifyContent="center">
+              <Grid item xs={12}>
+                <Typography variant="h5" align="center">
+                  Редактирование профиля
+                </Typography>
+              </Grid>
+
+              <Grid item xs={12}>
+                <TextField
+                  fullWidth
+                  label="Имя"
+                  variant="standard"
+                  name="name"
+                  type="text"
+                  required
+                  value
+                  onChange
+                />
+              </Grid>
+
+              <Grid item xs={12}>
+                <TextField
+                  fullWidth
+                  label="Пароль"
+                  variant="standard"
+                  type="password"
+                  name="password"
+                  required
+                  value
+                  onChange
+                />
+              </Grid>
+
+              <Grid item xs={12}>
+                <TextField
+                  fullWidth
+                  label="Фото профиля"
+                  variant="standard"
+                  type="file"
+                  name="img"
+                  required
+                  onChange
+                />
+              </Grid>
+            </Grid>
+            <div style={{ display: "flex", justifyContent: "space-around" }}>
+              <Grid sx={{ paddingTop: "5%" }}>
+                <Button
+                  variant="contained"
+                  color="warning"
+                  type="submit"
+                  onClick={handleClose}
+                >
+                  Close
+                </Button>
+                <Button variant="contained" color="primary" type="submit">
+                  Отправить
+                </Button>
+              </Grid>
+            </div>
+          </form>
         </Box>
       </Modal>
     </div>
