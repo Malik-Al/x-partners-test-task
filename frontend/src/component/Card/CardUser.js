a3bc90fbb8585ec2fa261271a134d2d1687b2d65
malik <malikimenov@gmail.com>
2024-04-21 17:33:02 +0600

    added filter date

--- frontend/src/component/Card/CardUser.js
+++ frontend/src/component/Card/CardUser.js
@@ -3,9 +3,16 @@ import config from "../../config.json";
 import { CardHeader, Avatar, CardContent, Typography } from "@mui/material";
 import "./style.css";
 
-
 export default function CardUser({ user }) {
   const { img, name, date_birth } = user;
+
+  const filterDate = (dateStr) => {
+    const date = new Date(dateStr);
+    const year = date.getFullYear();
+    const month = ("0" + (date.getMonth() + 1)).slice(-2);
+    const day = ("0" + date.getDate()).slice(-2);
+    return `${year}-${month}-${day}`;
+  };
   return (
     <div class="user-card">
       <CardHeader
@@ -14,7 +21,7 @@ export default function CardUser({ user }) {
       />
       <CardContent>
         <Typography variant="body2" color="textSecondary" component="p">
-          Дата рождения: {date_birth}
+          Дата рожд: {filterDate(date_birth)}
         </Typography>
       </CardContent>
     </div>
